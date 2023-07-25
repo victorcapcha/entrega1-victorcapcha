017179776a841bbd1b6f8dab63557132f7301419
victor <victor_14x_02s@hotmail.com>
2023-07-25 12:16:44 -0300

    agregando un siclo

--- js/main.js
+++ js/main.js
@@ -1,49 +1,58 @@
 let jugador;
 let pc;
 
-function aleatoria(){
-  return Math.ceil(Math.random()*3)
+function aleatoria() {
+  return Math.ceil(Math.random() * 3);
 }
 
-function eleccion(jugada){
-    let resultado = ''
-    if (jugada == 1){
-      resultado = 'PIEDRA'
-    }
-    else if (jugada == 2){
-      resultado = 'PAPEL'
-    }
-    else if (jugada == 3){
-      resultado = 'TIJERA'
-    }
-    else{
-      resultado = 'TU ELECCION ES INCORRECTA';
-    }
-    return resultado
+function eleccion(jugada) {
+  let resultado = '';
+  if (jugada == 1) {
+    resultado = 'PIEDRA';
+  } else if (jugada == 2) {
+    resultado = 'PAPEL';
+  } else if (jugada == 3) {
+    resultado = 'TIJERA';
+  } else {
+    resultado = 'TU ELECCION ES INCORRECTA';
+  }
+  return resultado;
+}
+
+let deseaVolverAJugar = true;
+
+while (deseaVolverAJugar) {
+  jugador = parseInt(prompt('Elija: 1 - PIEDRA, 2 - PAPEL, 3 - TIJERA'));
+
+  // bucle
+  while (isNaN(jugador) || jugador < 1 || jugador > 3) {
+    jugador = parseInt(prompt('Elección inválida. Elija: 1 - PIEDRA, 2 - PAPEL, 3 - TIJERA'));
   }
 
-jugador = prompt('elija: 1 - PIEDRA, 2 - PAPEL, 3 - TIJERA')
-pc = aleatoria();
-console.log('tu elejiste: '+ eleccion(jugador))
+  pc = aleatoria();
+  console.log('Tu elegiste: ' + eleccion(jugador));
 
+  if (eleccion(jugador) === 'TU ELECCION ES INCORRECTA') {
+    console.log('Intenta nuevamente.');
+  } else {
+    console.log('PC eligió: ' + eleccion(pc));
+    if (jugador === pc) {
+      console.log('EMPATE');
+    } else if (jugador === 1 && pc === 3) {
+      console.log('GANASTE');
+    } else if (jugador === 2 && pc === 1) {
+      console.log('GANASTE');
+    } else if (jugador === 3 && pc === 2) {
+      console.log('GANASTE');
+    } else {
+      console.log('PERDISTE');
+    }
+  }
 
-if ( eleccion(jugador) == 'TU ELECCION ES INCORRECTA'){
-    
-} else {
-    console.log('pc elijio: '+ eleccion(pc))
-    if(jugador == pc){
-        console.log('EMPATE')
-      }
-      else if(jugador ==1 && pc ==3){
-        console.log('GANASTE')
-      }
-      else if(jugador ==2 && pc ==1){
-        console.log('GANASTE')
-      }
-      else if(jugador ==3 && pc ==2){
-        console.log('GANASTE')
-      }
-      else {
-        console.log('PERDISTE')
-      }
+  // aca se preguntar al usuario si desea volver a jugar
+  const respuesta = prompt('¿Deseas volver a jugar? (s/n)');
+  deseaVolverAJugar = respuesta.toLowerCase() === 's';
 }
+
+console.log('Gracias por jugar. ¡Hasta luego!');
+
